7663696f7e983ce60a1447b70bcffb61688d44b1
franciscoMarquezBocanegra <francisco.marquez@mail.escuelaing.edu.co>
2024-03-18 23:41:50 -0500

    Entrega roundbound

---
+++ src/main/java/org/example/server/SparkWebServer.java
@@ -0,0 +1,28 @@
+package org.example.server;
+
+
+import org.example.services.LogServices;
+
+import static spark.Spark.*;
+
+
+
+public class SparkWebServer {
+
+    public static void main(String... args){
+        port(getPort());
+        staticFiles.location("/public");
+        get("/roundrobin", (req,res) -> {
+            String log = req.queryParams("log");
+            return LogServices.getLogs(log);
+        });
+    }
+
+    private static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567;
+    }
+
+}
